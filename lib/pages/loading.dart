2877f15593991bac3afa1e50cf90dfd568bca702
ammrhzzq <93185244+ammrhzzq@users.noreply.github.com>
2023-05-21 13:18:26 +0800

    Add files via upload

---
+++ lib/pages/loading.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWordTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Kuala Lumpur',
+        flag: 'malaysia.png',
+        latitude: '3.1319',
+        longitude: '101.6841');
+    await instance.getTime();
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWordTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: const Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
